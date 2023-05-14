0e61bc42cd6961944b3e2c69c1891d88d870b281
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-05-14 15:28:34 +0530

    Category highlighy not working check

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 # Create your views here.
 
 def items(request):
     query = request.GET.get('query', '')
+    categories = Category.objects.all()
+    category_id = request.GET.get('category', 0)
     items = Item.objects.filter(is_sold=False)
 
     if query:
-        items = items.filter(name__icontains=query)
+        items = items.filter(Q(name__icontains=query)|Q(description__icontains=query))
     
     return render(request, 'item/items.html', {
         'items': items,
         'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
     })
 
 def detail(request, pk):
@@ -22,7 +27,7 @@ def detail(request, pk):
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     return render(request, 'item/detail.html', {
         'item': item,
-        'related_items': related_items
+        'related_items': related_items,
     })
 
 @login_required
